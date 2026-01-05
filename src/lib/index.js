45cde6b092b94503fc581848c19a0c351201b5e6
Andre The Giant <andrecollin.toronto@gmail.com>
2026-01-05 15:24:12 -0500

    Form-Label

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,3 +5,4 @@ export { default as FormTextarea } from "./components/Form-Textarea/index.astro"
 export { default as FormSelect } from "./components/Form-Select/index.astro";
 export { default as FormCheckbox } from "./components/Form-Checkbox/index.astro";
 export { default as FormRadio } from "./components/Form-Radio/index.astro";
+export { default as FormLabel } from "./components/Form-Label/index.astro";
